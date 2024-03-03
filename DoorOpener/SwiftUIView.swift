c1f473f74f38acf76d3aa5a17b5f22b6980881bb
김지훈 <kim@jihun.io>
2024-03-03 19:56:38 +0900

    commit

---
+++ DoorOpener/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  DoorOpener
+//
+//  Created by 김지훈 on 3/3/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
