badd37383c9be3f2047c55322d2480fa0d6df3b4
김지훈 <lunarr@me.com>
2024-03-03 18:32:01 +0900

    Initial Commit

---
+++ DoorOpener/DoorOpenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorOpenerApp.swift
+//  DoorOpener
+//
+//  Created by 김지훈 on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DoorOpenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
