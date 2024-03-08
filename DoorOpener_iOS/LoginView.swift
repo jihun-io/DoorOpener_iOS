db7f0d12cbef37dfeff7661206427719bc264e85
김지훈 <kim@jihun.io>
2024-03-09 05:29:28 +0900

    로그인 UX 개선
    1. 로그인 기능 함수로 모듈화
    2. TextField 내 소프트웨어 키보드 submit 시 로그인 가능
    3. 로그인 중일 때 로딩 인디케이터 추가

--- DoorOpener_iOS/LoginView.swift
+++ DoorOpener_iOS/LoginView.swift
@@ -7,11 +7,79 @@
 import SwiftUI
 import Foundation
 
+func LoginProcess(email: String, password: String, userData: UserData) async -> String {
+    @AppStorage("openerURL") var openerURL = ""
+    var resultofLogin = "Not Logged in yet"
+
+    // 로그인 요청을 보냅니다.
+    let loginInfo = "email=\(email)&password=\(password)"
+    let loginData = loginInfo.data(using: .utf8)
+    let url = URL(string: "\(openerURL)/login")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = loginData
+    request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+    let config = URLSessionConfiguration.default
+    config.httpCookieStorage = HTTPCookieStorage.shared
+    let session = URLSession(configuration: config)
+
+    do {
+        let (data, response) = try await session.data(for: request)
+        let str = String(data: data, encoding: .utf8)
+
+        // 서버의 응답에서 로그인 성공 메시지를 찾습니다.
+        if str?.contains("<h1 class=\"status_message\">로그인을<br>완료했습니다.</h1>") == true {
+            // 사용자 정보 페이지를 요청합니다.
+            let url = URL(string: "\(openerURL)/settings/user")!
+            let (data, response) = try await URLSession.shared.data(from: url)
+            let str = String(data: data, encoding: .utf8)
+
+            // 응답에서 사용자 이름과 이메일을 파싱합니다.
+            if let nameRegex = try? NSRegularExpression(pattern: "<h1 id=\"name\">(.*)</h1>", options: []),
+               let emailRegex = try? NSRegularExpression(pattern: "<h2 id=\"email\">(.*)</h2>", options: []),
+               let str = str {
+                if let nameMatch = nameRegex.firstMatch(in: str, options: [], range: NSRange(location: 0, length: str.utf16.count)),
+                   let emailMatch = emailRegex.firstMatch(in: str, options: [], range: NSRange(location: 0, length: str.utf16.count)),
+                   let nameRange = Range(nameMatch.range(at: 1), in: str),
+                   let emailRange = Range(emailMatch.range(at: 1), in: str) {
+                    let name = String(str[nameRange])
+                    let email = String(str[emailRange])
+
+                    // 사용자 이름과 이메일을 저장합니다.
+                    UserDefaults.standard.set(name, forKey: "user_name")
+                    UserDefaults.standard.set(email, forKey: "user_email")
+                    userData.username = name
+                    userData.email = email
+                    print("userID 저장 완료")
+                    print("\(userData.username), \(userData.email)")
+                    print("로그인성공!!!!!!!!!!")
+                    UserDefaults.standard.set(true, forKey: "loginSuccessful")
+                    if UserDefaults.standard.bool(forKey: "loginSuccessful") {
+                        print("로그인값이 트루다!")
+                        await UIApplication.shared.registerForRemoteNotifications()
+                        resultofLogin = "Success"
+                    } else {
+                        resultofLogin = "Failed"
+                    }
+                }
+            }
+        }
+    } catch {
+        print("Error: \(error)")
+    }
+
+    return resultofLogin
+}
+
+
 struct LoginParent: View {
     @AppStorage("openerURL") var openerURL = ""
     @Binding var loginSuccessful: Bool
     @State private var isLinkActive = false
     @State private var showAlert = false
+    
+    @EnvironmentObject var userData: UserData
+
     
     func isValidURL(_ urlString: String) -> Bool {
         guard let url = URL(string: urlString),
@@ -47,11 +115,14 @@ struct LoginParent: View {
                 .alert(isPresented: $showAlert) {
                     Alert(title: Text("올바르지 않은 URL"), message: Text("올바른 URL을 입력하십시오."), dismissButton: .default(Text("확인")))
                 }
-                NavigationLink(destination: Login(loginSuccessful: $loginSuccessful), isActive: $isLinkActive) {
+                NavigationLink(destination: Login(loginSuccessful: $loginSuccessful).environmentObject(userData), isActive: $isLinkActive) {
                     EmptyView()
                 }
             }
             .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }
+        .onAppear {
+            UIApplication.shared.registerForRemoteNotifications()
         }
     }
 }
@@ -62,116 +133,100 @@ struct Login: View {
     @EnvironmentObject var userData: UserData
     @State private var email = ""
     @State private var password = ""
-    @State private var showingAlert = false  // 알림 표시 여부를 결정하는 새로운 @State 변수
+    @State private var showingAlert = false  // 알림 표시 여부를 결정하는 @State 변수
+    @State private var isLoading = false
     
     @AppStorage("openerURL") var openerURL = ""
     
     var body: some View {
-        VStack {
-            if loginSuccessful {
-                ParentView()
-            } else {
-                Text("DoorOpener")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
-                
-                Group {
-                    TextField("이메일", text: $email)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color(UIColor.secondarySystemBackground)))
+        ZStack {
+            VStack {
+                if loginSuccessful {
+                    ParentView()
+                } else {
+                    Text("DoorOpener")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
                     
-                    SecureField("비밀번호", text: $password)
-                        .padding()
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color(UIColor.secondarySystemBackground)))
-                }
-                
-                Button(action: {
-                    // 로그인 요청을 보냅니다.
-                    let loginInfo = "email=\(self.email)&password=\(self.password)"
-                    let loginData = loginInfo.data(using: .utf8)
-                    let url = URL(string: "\(openerURL)/login")!
-                    var request = URLRequest(url: url)
-                    request.httpMethod = "POST"
-                    request.httpBody = loginData
-                    request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-                    let config = URLSessionConfiguration.default
-                    config.httpCookieStorage = HTTPCookieStorage.shared
-                    let session = URLSession(configuration: config)
-                    let task = session.dataTask(with: request) { (data, response, error) in
-                        if let error = error {
-                            print("Error: \(error)")
-                        } else if let httpResponse = response as? HTTPURLResponse {
-                            print("Received headers:\n\(httpResponse.allHeaderFields)")
-                        }
-                        if let data = data {
-                            let str = String(data: data, encoding: .utf8)
-                            //                            print("Received data 1:\n\(str ?? "")")
-                            // 서버의 응답에서 로그인 성공 메시지를 찾습니다.
-                            if str?.contains("<h1 class=\"status_message\">로그인을<br>완료했습니다.</h1>") == true {
-                                // 사용자 정보 페이지를 요청합니다.
-                                let url = URL(string: "\(openerURL)/settings/user")!
-                                let task = session.dataTask(with: url) { (data, response, error) in
-                                    if let error = error {
-                                        print("Error: \(error)")
-                                    } else if let data = data {
-                                        let str = String(data: data, encoding: .utf8)
-                                        // 응답에서 사용자 이름과 이메일을 파싱합니다.
-                                        if let nameRegex = try? NSRegularExpression(pattern: "<h1 id=\"name\">(.*)</h1>", options: []),
-                                           let emailRegex = try? NSRegularExpression(pattern: "<h2 id=\"email\">(.*)</h2>", options: []),
-                                           let str = str {
-                                            if let nameMatch = nameRegex.firstMatch(in: str, options: [], range: NSRange(location: 0, length: str.utf16.count)),
-                                               let emailMatch = emailRegex.firstMatch(in: str, options: [], range: NSRange(location: 0, length: str.utf16.count)),
-                                               let nameRange = Range(nameMatch.range(at: 1), in: str),
-                                               let emailRange = Range(emailMatch.range(at: 1), in: str) {
-                                                let name = String(str[nameRange])
-                                                let email = String(str[emailRange])
-                                                
-                                                DispatchQueue.main.async {
-                                                    // 사용자 이름과 이메일을 저장합니다.
-                                                    UserDefaults.standard.set(name, forKey: "user_name")
-                                                    UserDefaults.standard.set(email, forKey: "user_email")
-                                                    userData.username = name
-                                                    userData.email = email
-                                                    print("userID 저장 완료")
-                                                    print("\(userData.username), \(userData.email)")
-                                                    UserDefaults.standard.set(true, forKey: "loginSuccessful")
-                                                    self.loginSuccessful = true
-                                                    print("로그인성공!!!!!!!!!!")
-                                                    if UserDefaults.standard.bool(forKey: "loginSuccessful") {
-                                                        print("로그인값이 트루다!")
-                                                        UIApplication.shared.registerForRemoteNotifications()
-                                                    }
-                                                }
-                                            }
+                    Group {
+                        TextField("이메일", text: $email)
+                            .padding()
+                            .keyboardType(/*@START_MENU_TOKEN@*/.emailAddress/*@END_MENU_TOKEN@*/)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color(UIColor.secondarySystemBackground)))
+                        
+                        SecureField("비밀번호", text: $password)
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color(UIColor.secondarySystemBackground)))
+                            .submitLabel(.join)
+                            .onSubmit {
+                                isLoading = true
+                                Task {
+                                    let result = await LoginProcess(email: email, password: password, userData: userData)
+                                    isLoading = false
+                                    if result == "Success" {
+                                        print("Success!!!! : \(userData.username), \(userData.email)")
+                                        
+                                        DispatchQueue.main.async {
+                                            loginSuccessful = true
                                         }
+                                    } else if result == "Not Logged in Yet"{
+                                        
+                                    } else {
+                                        print(result)
+                                        showingAlert = true
                                     }
                                 }
-                                task.resume()
-                            } else {
+                            }
+                    }
+                    
+                    Button(action: {
+                        isLoading = true
+                        Task {
+                            let result = await LoginProcess(email: email, password: password, userData: userData)
+                            
+                            isLoading = false
+                            if result == "Success" {
+                                print("Success!!!! : \(userData.username), \(userData.email)")
+                                
                                 DispatchQueue.main.async {
-                                    self.showingAlert = true  // 로그인 실패 시 알림을 표시
+                                    loginSuccessful = true
                                 }
+                            } else if result == "Not Logged in Yet"{
+                                
+                            } else {
+                                print(result)
+                                showingAlert = true
                             }
                         }
+                    }) {
+                        Text("로그인")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 100)
+                            .background(Color.blue)
+                            .cornerRadius(10)
                     }
-                    task.resume()
-                }) {
-                    Text("로그인")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 100)
-                        .background(Color.blue)
-                        .cornerRadius(10)
+                    .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
+                    .alert(isPresented: $showingAlert) {  // 알림을 표시하는 alert 수정자
+                        Alert(title: Text("로그인 실패"), message: Text("이메일과 비밀번호를 다시 확인해주세요."), dismissButton: .default(Text("확인")))
+                    }
                 }
-                .padding(/*@START_MENU_TOKEN@*/.all, 30.0/*@END_MENU_TOKEN@*/)
-                .alert(isPresented: $showingAlert) {  // 알림을 표시하는 alert 수정자
-                    Alert(title: Text("로그인 실패"), message: Text("이메일과 비밀번호를 다시 확인해주세요."), dismissButton: .default(Text("확인")))
-                }
-                
+            }
+            .padding()
+
+            if isLoading {
+                Color.black.opacity(0.4)
+                    .ignoresSafeArea()
+                    .onTapGesture { }
+            }
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .scaleEffect(2)
+//                    .opacity(isLoading ? 1 : 0)
             }
         }
-        .padding()
     }
 }
 
@@ -184,7 +239,7 @@ struct LoginViewPreview: PreviewProvider {
         //        Settings(loginSuccessful: $loginSuccessful)
         //        OpenLogsView()
         //        Login(loginSuccessful: $loginSuccessful)
-        LoginParent(loginSuccessful: $loginSuccessful)
+        Login(loginSuccessful: $loginSuccessful)
         
         
             .environmentObject(UserData())
